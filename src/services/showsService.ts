3fe7d8414dbd2c017e5fc3a17c0ed399e3df2044
Bruno Cantero <47759015+BrunoCantero@users.noreply.github.com>
2021-08-10 15:38:55 -0300

    show service

---
+++ src/services/showsService.ts
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+// get shows
+export const getShows = () => {
+  return new Promise<any>(async (resolve, reject) => {
+    console.log(process.env.REACT_APP_API_URL, 'url')
+    try {
+      const response = await axios.get(
+        'http://api.tvmaze.com/search/shows?q=girls',
+      )
+      resolve(response.data)
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
+// get images by show id
+export const getImages = (id: number) => {
+  return new Promise<any>(async (resolve, reject) => {
+    try {
+      const response = await axios.get(
+        `https://api.tvmaze.com/shows/${id}/images`,
+      )
+      resolve(response.data)
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
